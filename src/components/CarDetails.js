5a4ef8c06bf11bde3ddcadc4b3b1decd07de8bbf
github username <github email address>
2019-04-16 09:00:49 -0400

    rendering edit form, need to make own component

--- src/components/CarDetails.js
+++ src/components/CarDetails.js
@@ -4,8 +4,12 @@ import { Button, Header, Image, Modal } from 'semantic-ui-react'
 const CarDetails = (props) => {
 
   return (
-    <Modal trigger={<Button> Show {props.car.vehicle_make}</Button>}>
-    </Modal>
+      <div>
+        <p> Transmission: {props.transmission} </p>
+        <p> Mileage: {props.mileage} </p>
+        <p> {props.description} </p>
+        <img src={props.image} />
+      </div>
     )
 
 }
